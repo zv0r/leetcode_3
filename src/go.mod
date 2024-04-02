bacba823de6437d0db59eb105ba8ccbe3cca0153
Andrey Zvorygin <zv0r@ya.ru>
2024-04-02 22:14:47 +0300

    Make dumb solution

---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module github.com/zv0r/leetcode_3
+
+go 1.19
---
+++ src/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(lengthOfLongestSubstring("abcabcbbabcdefgh"), lengthOfLongestSubstring("abcabcbbabcdefgh") == 8)
+	fmt.Println(lengthOfLongestSubstring("abcabcbb"), lengthOfLongestSubstring("abcabcbb") == 3)
+	fmt.Println(lengthOfLongestSubstring("bbbbb"), lengthOfLongestSubstring("bbbbb") == 1)
+	fmt.Println(lengthOfLongestSubstring("pwwkew"), lengthOfLongestSubstring("pwwkew") == 3)
+	fmt.Println(lengthOfLongestSubstring("dvdf"), lengthOfLongestSubstring("dvdf") == 3)
+	fmt.Println(lengthOfLongestSubstring("au"), lengthOfLongestSubstring("au") == 2)
+	fmt.Println(lengthOfLongestSubstring("a"), lengthOfLongestSubstring("a") == 1)
+	fmt.Println(lengthOfLongestSubstring(""), lengthOfLongestSubstring("") == 0)
+}
+
+func lengthOfLongestSubstring(s string) int {
+	max_unique_length := 0
+	if len(s) > 1 {
+	check_unique_substr:
+		for i := 0; i < len(s); i++ {
+			unique_substr := make(map[byte]bool)
+			curr_unique_length := 0
+			for j := i; j < len(s); j++ {
+				symbol := s[j]
+				_, symbol_exists := unique_substr[symbol]
+				if symbol_exists {
+					break
+				} else {
+					unique_substr[symbol] = true
+					curr_unique_length++
+					if curr_unique_length > max_unique_length {
+						max_unique_length = curr_unique_length
+					}
+					if j == len(s)-1 {
+						break check_unique_substr
+					}
+				}
+			}
+		}
+	} else {
+		max_unique_length = len(s)
+	}
+	return max_unique_length
+}
