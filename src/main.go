154b8f7008727769d986759cc385e44300a8cc20
Andrey Zvorygin <zv0r@ya.ru>
2024-04-04 15:53:08 +0300

    Add not so dumb solution

--- src/main.go
+++ src/main.go
@@ -13,32 +13,49 @@ func main() {
 	fmt.Println(lengthOfLongestSubstring(""), lengthOfLongestSubstring("") == 0)
 }
 
+// func lengthOfLongestSubstring(s string) int {
+// 	max_unique_length := 0
+// 	if len(s) > 1 {
+// 	check_unique_substr:
+// 		for i := 0; i < len(s); i++ {
+// 			unique_substr := make(map[byte]bool)
+// 			curr_unique_length := 0
+// 			for j := i; j < len(s); j++ {
+// 				symbol := s[j]
+// 				_, symbol_exists := unique_substr[symbol]
+// 				if symbol_exists {
+// 					break
+// 				} else {
+// 					unique_substr[symbol] = true
+// 					curr_unique_length++
+// 					if curr_unique_length > max_unique_length {
+// 						max_unique_length = curr_unique_length
+// 					}
+// 					if j == len(s)-1 {
+// 						break check_unique_substr
+// 					}
+// 				}
+// 			}
+// 		}
+// 	} else {
+// 		max_unique_length = len(s)
+// 	}
+// 	return max_unique_length
+// }
+
 func lengthOfLongestSubstring(s string) int {
 	max_unique_length := 0
-	if len(s) > 1 {
-	check_unique_substr:
-		for i := 0; i < len(s); i++ {
-			unique_substr := make(map[byte]bool)
-			curr_unique_length := 0
-			for j := i; j < len(s); j++ {
-				symbol := s[j]
-				_, symbol_exists := unique_substr[symbol]
-				if symbol_exists {
-					break
-				} else {
-					unique_substr[symbol] = true
-					curr_unique_length++
-					if curr_unique_length > max_unique_length {
-						max_unique_length = curr_unique_length
-					}
-					if j == len(s)-1 {
-						break check_unique_substr
-					}
-				}
-			}
+	lastOccurred := make(map[rune]int)
+	start := 0
+
+	for i, ch := range s {
+		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+			start = lastI + 1
 		}
-	} else {
-		max_unique_length = len(s)
+		if i-start+1 > max_unique_length {
+			max_unique_length = i - start + 1
+		}
+		lastOccurred[ch] = i
 	}
 	return max_unique_length
 }
